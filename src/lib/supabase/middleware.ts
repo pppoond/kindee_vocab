d15331851826f8d0846bd2091e4a64038f66d637
Siwat Laothong <siwat@builk.com>
2026-03-03 17:47:26 +0700

    allow google

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -40,7 +40,8 @@ export async function updateSession(request: NextRequest) {
     request.nextUrl.pathname !== '/' &&
     !request.nextUrl.pathname.startsWith('/login') &&
     !request.nextUrl.pathname.startsWith('/auth') &&
-    !request.nextUrl.pathname.startsWith('/donate')
+    !request.nextUrl.pathname.startsWith('/donate') &&
+    !request.nextUrl.pathname.startsWith('/googlee9b399be387fb935.html')
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
